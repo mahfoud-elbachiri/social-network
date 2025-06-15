fa3d1abd536dd64407f8641878ae07c45ac22904
mahfoud-elbachiri <mahfoudelbachiri@gmail.com>
2025-06-15 19:16:18 +0100

    e

--- javascript/errore.js
+++
@@ -1,7 +0,0 @@
-export function showError(message, id) {
-    let err = document.getElementById(id)
-    
-    err.textContent = message
-    err.style.display = "block"
-    err.style.color = "red"
-}
