7868cebefa84c3f067b856925cc0013b1ef9aca6
Mac <jebbari635@gmail.com>
2025-06-17 14:13:29 +0100

    rani zedt folow btn f explor users

--- src/app/Explore/page.jsx
+++ src/app/Explore/page.jsx
@@ -133,7 +133,9 @@ export default function Explore() {
                         </span>
                       </div>
                     </div>
-                   
+                    <button className="follow-btn">
+                      Follow
+                    </button>
                   </div>
                 ))}
               </div>
