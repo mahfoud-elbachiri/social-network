b4fa1d4f6e7dd31e16630fa1fb290398bc036c0a
Mac <jebbari635@gmail.com>
2025-06-24 22:09:20 +0100

    fixe pading/accepted stats with notifications

--- src/components/FollowButton.jsx
+++ src/components/FollowButton.jsx
@@ -5,7 +5,6 @@ import { FaUserPlus, FaUserXmark,FaUserClock } from 'react-icons/fa6';
 
  const FollowButton = ({ targetUserid ,isPrivateView}) =>{
   const [following, setFollowing] = useState(null);
-const [isPrivat , setIsprivat] = useState(null)
 const [IsPanding , setIsPanding] = useState(null)
 
 
@@ -22,15 +21,14 @@ const [IsPanding , setIsPanding] = useState(null)
           console.log("vivo:",isPrivateView);
           
           setFollowing(data.isFollowing)
-          setIsprivat(data.isPrivat)
           setIsPanding(data.IsPanding)
         }catch (err){
           console.log("error while cheking follow satus:",err);
           
-        }
-      }
+        }      }
     checkfollow()
   },[targetUserid , isPrivateView])
+
   const handleClick = async () => {
     console.log("1",targetUserid);
     try {
@@ -50,14 +48,13 @@ const [IsPanding , setIsPanding] = useState(null)
       if (!response.ok) {
         console.log("response:",await response.json());
         throw new Error('Request failed');
-      } else {
-        if (following) {
+      } else {        if (following) {
           // User is unfollowing
           setFollowing(false);
           setIsPanding(false);
         } else {
           // User is following
-          if (isPrivat) {
+          if (isPrivateView) {
             // Private profile - set as pending
             setFollowing(false);
             setIsPanding(true);
@@ -78,49 +75,31 @@ const getButtonClass = () => {
     if (IsPanding) return 'follow-button pending';
     if (following) return 'follow-button following';
     return 'follow-button';
-  };
-  const buttonStatu =  () =>{
-    /*
-  console.log("ispanding :" , IsPanding );
-  console.log("following : :" , following );
- console.log("isprivat : :" , isPrivat );
-*/
-    if (isPrivateView) {
-
-       if (!following) { 
-          return (
-            <>
-              <FaUserPlus /> Follow
-            </>
-           )
-
-       }else {
-       
-         return (
-               <>
-              <FaUserClock /> panding
-              </>
-           )
-       }
-       
-    }
-   
-    if (!following) {
-        return (
-         <>
-          <FaUserPlus /> Follow
+  };  const buttonStatu =  () =>{
+    // If pending, show pending status
+    if (IsPanding) {
+      return (
+        <>
+          <FaUserClock /> Pending
         </>
-       )
+      );
     }
+
+    // If following, show unfollow
     if (following) {
-       return (
-         <>
+      return (
+        <>
           <FaUserXmark /> Unfollow
         </>
-       )
+      );
     }
-    
 
+    // If not following, show follow
+    return (
+      <>
+        <FaUserPlus /> Follow
+      </>
+    );
   }
 if (following === null) return null;
 
